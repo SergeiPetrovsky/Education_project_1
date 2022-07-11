437cbf46608c1e36a7b0405e783d6d3e395e32a7
Сергей <petrovskysergei72@gmail.com>
2022-07-11 13:54:34 +0300

    start_files

---
+++ Sergei_2021/blog/views.py
@@ -0,0 +1,20 @@
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Post, Comment
+
+
+def blog_index(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog_index.html', context)
+
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog_detail.html', context)
