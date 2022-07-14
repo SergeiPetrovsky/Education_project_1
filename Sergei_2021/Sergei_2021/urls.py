1842e83aa795397f13e4d840e994f1a57763dbbc
Сергей <petrovskysergei72@gmail.com>
2022-07-14 10:40:01 +0300

    start_files

--- Sergei_2021/Sergei_2021/urls.py
+++ Sergei_2021/Sergei_2021/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('project.urls')),
-    path('blog_index', include('blog.urls'))
+    path('blog/', include('blog.urls'))
 ]
